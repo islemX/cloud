584c35d32acad9439b8862c8ff26b8adce43f0d9
islemX <islembvenamor1110@gmail.com>
2022-10-08 01:31:57 +0200

    cloud

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
-function App() {
+
+
+function Children() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className="App">
+     <div class="mapouter">
+      <div class="gmap_canvas">
+      <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=douz&t=k&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+      <a href="https://123movies-to.org"></a>
+      <br/>
+      
+      <a href="https://www.embedgooglemap.net">google maps for free</a>
+    </div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+function Parent() {
+  const [isMouseOver, setIsMouseOver] = useState(false);
+
+  const handleEnter = () => {
+    setIsMouseOver(true);
+  };
+
+  const handleLeave = () => {
+    setIsMouseOver(false);
+  };
+  return (
+    <div onMouseEnter={handleEnter} onMouseLeave={handleLeave} className="hov4">
+   <h2 style={{textAlign:"center",border:"solid rgb(218,165,32) ",borderRadius:"15px",width:"500px",marginLeft:"35%"}}>HOVER TO SEE MY HOME'S LOCATION</h2>
+      {isMouseOver ? (
+        <>
+          <Children />
+        </>
+      ) : null}
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+   
+      <Parent/>
+    
+  );
+}
